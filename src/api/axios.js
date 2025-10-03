31d765b86d744e72d8f6ed2d2cac1499167f077c
vijay-cmtai <vijay.maurya@completrix.com>
2025-10-03 16:26:34 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://jaivicbackend.vercel.app/api/v1",
 });
 
 API.interceptors.request.use((req) => {
